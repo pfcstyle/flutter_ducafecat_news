c592d00d01047c887e29f66972aba7c02d65a627
Will <475756218@qq.com>
2022-08-08 22:53:42 +0800

    1.0.4

---
+++ lib/common/values/storage.dart
@@ -0,0 +1,12 @@
+/// 用户 - 配置信息
+const String STORAGE_USER_PROFILE_KEY = 'user_profile';
+
+/// 设备是否第一次打开
+const String STORAGE_DEVICE_ALREADY_OPEN_KEY = 'device_already_open';
+
+/// 首页新闻cacheKey
+const String STORAGE_INDEX_NEWS_CACHE_KEY = 'cache_index_news';
+
+const String STORAGE_MASTER_KEY = 'ducafcat_news_app';
+const String STORAGE_USER_TOKEN_KEY = 'user_token';
+
